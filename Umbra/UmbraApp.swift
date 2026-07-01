f2fbab12c05c28032085ee9022a600a616b9a7ea
Nasrulloh <ilmi.nasrulloh@gmail.com>
2026-07-01 10:26:37 +0700

    Initial Commit

---
+++ Umbra/UmbraApp.swift
@@ -0,0 +1,17 @@
+//
+//  UmbraApp.swift
+//  Umbra
+//
+//  Created by M Ilmi Nasrulloh on 01/07/26.
+//
+
+import SwiftUI
+
+@main
+struct UmbraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
