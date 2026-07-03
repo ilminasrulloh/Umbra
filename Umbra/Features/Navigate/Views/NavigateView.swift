5c11e89e93e912cae7af8f7d956a60579d60f862
Nasrulloh <ilmi.nasrulloh@gmail.com>
2026-07-03 13:48:54 +0700

    feat: add NavigateView

--- Umbra/Features/Navigate/Views/NavigateView.swift
+++ Umbra/Features/Navigate/Views/NavigateView.swift
@@ -6,10 +6,69 @@
 //
 
 import SwiftUI
+import Combine
+import MapKit
 
 struct NavigateView: View {
+    @StateObject private var locationManager = UserLocationManager()
+    @State private var userCurrentPosition: MapCameraPosition = .userLocation(fallback: .automatic)
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        ZStack (alignment: .top) {
+            
+            Map(position: $userCurrentPosition) {
+                UserAnnotation()
+            }
+            .ignoresSafeArea()
+            .onAppear {
+                locationManager.RequestUserLocation()
+            }
+            
+            VStack {
+                
+                HStack(spacing: 16) {
+                    Image(systemName: "arrow.turn.up.left")
+                        .font(.system(size: 48))
+                    
+                    VStack(alignment: .leading, spacing: 4) {
+                        Text("65 m")
+                            .font(.headline)
+                            .foregroundColor(.black)
+                        
+                        Text("Turn left onto the path")
+                            .font(.caption)
+                            .foregroundColor(.gray)
+                    }
+                    
+                    Spacer()
+                }
+                .frame(width: 340)
+                .padding(16)
+                .background(Color.white.opacity(0.90))
+                .cornerRadius(24)
+                
+                Spacer()
+                
+                HStack(spacing: 16) {
+                    VStack(alignment: .leading, spacing: 4) {
+                        Text("7 mins")
+                            .font(.headline)
+                            .foregroundColor(.black)
+                        
+                        Text("9:50 AM . 700 m")
+                            .font(.caption)
+                            .foregroundColor(.gray)
+                    }
+                    
+                    Spacer()
+                }
+                .frame(width: 340)
+                .padding(16)
+                .background(Color.white.opacity(0.90))
+                .cornerRadius(24)
+            }
+        }
     }
 }
 
